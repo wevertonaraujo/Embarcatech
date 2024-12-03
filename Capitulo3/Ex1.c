727963bf2ef45d7dc64f6c70c3d37997a91e4c14
Ivanilson França Vieira Junior <ivanilson.junior@ifrn.edu.br>
2024-12-03 22:54:46 +0000

    Exemplo inicial de olá mundo

---
+++ Capitulo3/Ex1.c
@@ -0,0 +1,10 @@
+//incluindo a bibliteca stdio
+#include <stdio.h>
+
+/*Função principal, ao executar um programa ela será a primeira a ser executada,
+ela deve ter um retorno do tipo inteiro (0 de retorno ele executou sem problemas, outro número pode indicar ao S.O. algum problema.) */
+int main() {
+    //Imprime o conteúdo no terminal. O \n é para ele inserir uma quebra de linha ao final
+    printf("Olá Embarcatech!\n");
+    return 0;
+}
