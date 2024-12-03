1bc5ecc09a0145fd8f7192d1bebabae90854dd73
Ivanilson França Vieira Junior <ivanilson.junior@ifrn.edu.br>
2024-12-03 23:11:38 +0000

    Operadores Aritiméticos

---
+++ Capitulo3/Ex2.c
@@ -0,0 +1,12 @@
+//Operadores Aritiméticos
+#include <stdio.h>
+int main(){
+    int a = 10;
+    int b = 2;
+    printf("O resultado de %i + %i é: %i \n", a, b, a+b);
+    printf("O resultado de %i - %i é: %i \n", a, b, a-b);
+    printf("O resultado de %i * %i é: %i \n", a, b, a*b);
+    printf("O resultado de %i / %i é: %i \n", a, b, a/b);
+    printf("O resultado de %i mod %i é: %i \n", a, b, a%b);
+    return 0;
+}
